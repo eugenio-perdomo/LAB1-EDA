52469baa5cc0cfc54cb5c4e2f7ac29fccaa55d8a
Frankb-eng <55456165+Frankb-eng@users.noreply.github.com>
2019-09-19 19:45:55 -0300

    Update Definiciones.h

--- Definiciones.h
+++ Definiciones.h
@@ -1,6 +1,33 @@
 #ifndef DEFINICIONES_H_INCLUDED
 #define DEFINICIONES_H_INCLUDED
 
+typedef enum _retorno{
+    OK,ERROR,NO_IMPLEMENTADO
+                     }TipoRet;
+
+typedef struct _archivo{
+    string nom;
+    string contenido[LARGO_MAX];
+    _archivo *ptrsig;
+                       }*Archivo;
+
+typedef struct _sistema{
+    string nom;
+    Archivo contenido;
+    //_sistema *subdir;
+    _sistema *dirsig;
+                       }*Sistema;
+
+
+//tipo1
+TipoRet DIR(Sistema s);
+
+//tipo2
+
+//opcionales
+
+//otras
+int tamanio(Archivo a);
 
 
 #endif // DEFINICIONES_H_INCLUDED
