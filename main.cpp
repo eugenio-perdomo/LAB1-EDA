be1d3b1294b63448806740dde230924475aac12e
eugenio-perdomo <scnhp.0894@gmail.com>
2019-10-01 12:02:19 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <string.h>
 #include "Definiciones.h"
-#include "Funciones.cpp" //incluir estas 2 aca puede causar problemas con el namespace std
+#include "Funciones.cpp"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    bool flag = true;
+    string palabra;
+    while(flag == true){
+        getline(cin,palabra,'\n');
+
+        if(palabra.compare("DIR") == 0)
+        if(palabra.compare("CREATE") == 0)
+        if(palabra.compare("IF") == 0)
+        if(palabra.compare("TYPE") == 0)
+        if(palabra.compare("DELETE") == 0)
+        if(palabra.compare("BF") == 0)
+        if(palabra.compare("CAT") == 0)
+        if(palabra.compare("IC") == 0)
+        if(palabra.compare("BC") == 0)
+        if(palabra.compare("UNDELETE") == 0)
+        if(palabra.compare("FIN") == 0)
+        flag = false;
+    }
     return 0;
 }
