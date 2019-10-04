09d18fda31e99b6a18432370ae20954f8f2fc24d
Bruno_S <55509590+K1uty@users.noreply.github.com>
2019-10-04 09:44:28 -0300

    Update Definiciones.h

--- Definiciones.h
+++ Definiciones.h
@@ -1,6 +1,6 @@
 #ifndef DEFINICIONES_H_INCLUDED
 #define DEFINICIONES_H_INCLUDED
-
+#define LARGO_MAX 10
 typedef enum _retorno{
     OK,ERROR,NO_IMPLEMENTADO
                      }TipoRet;
@@ -9,7 +9,7 @@ typedef enum _retorno{
 
 typedef struct _archivo{
     char nombreArchivo[19];
-    char contenido[10]; //LARGO_MAX
+    char contenido[LARGO_MAX]; //LARGO_MAX
     _archivo *ptrsig;
                        }*Archivo;
 
@@ -28,7 +28,7 @@ TipoRet IF();
 TipoRet TYPE();
 
 //tipo2
-TipoRet DELETE();
+TipoRet DELETE(Directorio d, char palabra[]);
 TipoRet BF();
 TipoRet CAT();
 
@@ -41,5 +41,7 @@ TipoRet UNDELETE();
 void MuestroRetorno(TipoRet ret);
 int tamanio(Archivo a);
 Directorio CrearArchivo(Directorio d, char nombre[]);
+Directorio eliminarArchivo(Directorio d, char nombre[]);
+bool esVacio(Archivo a);
 
 #endif // DEFINICIONES_H_INCLUDED
