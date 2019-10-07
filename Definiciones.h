54d9fd1af6b034f0c7b5798469753a71837294d5
eugenio-perdomo <scnhp.0894@gmail.com>
2019-10-07 18:59:59 -0300

    Agregadas varias Definiciones

--- Definiciones.h
+++ Definiciones.h
@@ -3,41 +3,45 @@
 
 typedef enum _retorno{
     OK,ERROR,NO_IMPLEMENTADO
-                     }TipoRet;
+}TipoRet;
+
+//typedef struct *Cadena;
 
 typedef struct _archivo{
-    string nom;
-    string contenido[LARGO_MAX];
+    char nombreArchivo[LARGONOMBRE];
+    char contenido[LARGO_MAX];
     _archivo *ptrsig;
-                       }*Archivo;
+}*Archivo;
 
-typedef struct _sistema{
-    string nom;
+typedef struct _directorio{
+    //std::string nombreDirectorio;
     Archivo contenido;
-    //_sistema *subdir;
-    _sistema *dirsig;
-                       }*Sistema;
+    _directorio *dirsig;
+}*Directorio;
 
 
 //tipo1
-TipoRet DIR(Sistema s);
-TipoRet CREATE();
-TipoRet IF();
-TipoRet TYPE();
+TipoRet DIR(Directorio d);
+TipoRet CREATE(Directorio d);
+TipoRet IF(Directorio d, std::string nombreArchivo, std::string texto);
+TipoRet TYPE(Directorio d, std::string nombreArchivo);
 
 //tipo2
-TipoRet DELETE();
+TipoRet DELETE(Directorio d, char palabra[]);
 TipoRet BF();
 TipoRet CAT();
 
 //opcionales
-TipoRet IC();
+TipoRet IC(Directorio d, std::string nombreArchivo, std::string texto);
 TipoRet BC();
 TipoRet UNDELETE();
 
 //otras
 void MuestroRetorno(TipoRet ret);
 int tamanio(Archivo a);
-
+Directorio CrearArchivo(Directorio d, char nombre[]);
+Directorio eliminarArchivo(Directorio d, char nombre[]);
+bool esVacio(Archivo a);
+bool hayComillas(std::string texto);
 
 #endif // DEFINICIONES_H_INCLUDED
