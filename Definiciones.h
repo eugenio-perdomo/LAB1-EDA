559e95dd2f2290a65f4a472a49b23d0936f8f4d5
Frankb-eng <55456165+Frankb-eng@users.noreply.github.com>
2019-10-11 17:55:53 -0300

    Update Definiciones.h

--- Definiciones.h
+++ Definiciones.h
@@ -1,48 +1,51 @@
 #ifndef DEFINICIONES_H_INCLUDED
 #define DEFINICIONES_H_INCLUDED
 
-typedef enum _retorno{
+
+typedef enum _retorno
+{
     OK,ERROR,NO_IMPLEMENTADO
-                     }TipoRet;
+} TipoRet;
 typedef char *Cadena;
 
-typedef struct _archivo{
-    char nombreArchivo[LARGONOMBRE];
+typedef struct _archivo
+{
+    std::string nombreArchivo;
     Cadena contenido[LARGO_MAX];
     _archivo *ptrsig;
     int lineas;
-                       }*Archivo;
+}*Archivo;
 
-typedef struct _directorio{
-    string nom;
+typedef struct _directorio
+{
+    std::string nom;
     Archivo contenido;
     //_directorio *subdir;
     _directorio *dirsig;
-                       }*Directorio;
-
+}*Directorio;
 
 //tipo1
 TipoRet DIR(Directorio d);
-TipoRet CREATE(Directorio d,  char nombre_archivo[]);
-TipoRet IF(Directorio d,  std::string nombreArchivo[],  std::string texto);
-TipoRet TYPE(Directorio d,  std::string nombreArchivo);
+TipoRet CREATE(Directorio d, std::string nombre_archivo);
+TipoRet IF(Directorio d, std::string nombreArchivo, std::string texto);
+TipoRet TYPE(Directorio d, std::string nombreArchivo);
 
 //tipo2
-TipoRet DELETE(Directorio d, char palabra[]);
-TipoRet BF();
+TipoRet DELETE(Directorio d, std::string palabra);
+TipoRet BF(Directorio d,string nombreArchivo, int linea);
 TipoRet CAT();
 
 //opcionales
-TipoRet IC(Directorio d,  std::string nombreArchivo, string texto);
+TipoRet IC(Directorio d, std::string nombreArchivo, string texto);
 TipoRet BC();
 TipoRet UNDELETE();
 
 //otras
 void MuestroRetorno(TipoRet ret);
 int tamanio(Archivo a);
-bool hayComillas(string texto);
-Directorio CrearArchivo(Directorio d, char nombre_archivo[]);
-Directorio eliminarArchivo(Directorio d, char nombre[]);
+bool hayComillas(std::string texto);
+Directorio CrearArchivo(Directorio d, std::string nombre_archivo);
+Directorio eliminarArchivo(Directorio d, std::string nombre);
 bool esVacio(Archivo a);
 
 
