9d064f03b7d51fceab4a9efa91de8fb75957000d
Bruno_S <55509590+K1uty@users.noreply.github.com>
2019-10-10 22:34:07 -0300

    Update Definiciones.h

--- Definiciones.h
+++ Definiciones.h
@@ -1,47 +1,50 @@
 #ifndef DEFINICIONES_H_INCLUDED
 #define DEFINICIONES_H_INCLUDED
 
-typedef enum _retorno{
+typedef enum _retorno
+{
     OK,ERROR,NO_IMPLEMENTADO
-}TipoRet;
-
+} TipoRet;
 typedef char *Cadena;
 
-typedef struct _archivo{
-    char nombreArchivo[LARGONOMBRE];
+typedef struct _archivo
+{
+    std::string nombreArchivo;
     Cadena contenido[LARGO_MAX];
     _archivo *ptrsig;
+    int lineas;
 }*Archivo;
 
-typedef struct _directorio{
-    //std::string nombreDirectorio;
+typedef struct _directorio
+{
+    std::string nom;
     Archivo contenido;
+    //_directorio *subdir;
     _directorio *dirsig;
 }*Directorio;
 
-
 //tipo1
 TipoRet DIR(Directorio d);
-TipoRet CREATE(Directorio d,char nombre_archivo[]);
-TipoRet IF(Directorio d, char nombreArchivo[], std::string texto);
-TipoRet TYPE(Directorio d, char nombreArchivo[]);
+TipoRet CREATE(Directorio d, std::string nombre_archivo);
+TipoRet IF(Directorio d, std::string nombreArchivo, std::string texto);
+TipoRet TYPE(Directorio d, std::string nombreArchivo);
 
 //tipo2
-TipoRet DELETE(Directorio d, char palabra[]);
+TipoRet DELETE(Directorio d, std::string palabra);
 TipoRet BF();
 TipoRet CAT();
 
 //opcionales
-TipoRet IC(Directorio d, std::string nombreArchivo, std::string texto);
+TipoRet IC(Directorio d, std::string nombreArchivo, string texto);
 TipoRet BC();
 TipoRet UNDELETE();
 
 //otras
 void MuestroRetorno(TipoRet ret);
 int tamanio(Archivo a);
-Directorio CrearArchivo(Directorio d, char nombre[]);
-Directorio eliminarArchivo(Directorio d, char nombre[]);
-bool esVacio(Archivo a);
 bool hayComillas(std::string texto);
+Directorio CrearArchivo(Directorio d, std::string nombre_archivo);
+Directorio eliminarArchivo(Directorio d, std::string nombre);
+bool esVacio(Archivo a);
 
 #endif // DEFINICIONES_H_INCLUDED
