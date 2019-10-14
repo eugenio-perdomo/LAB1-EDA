2d79906b65b84c3d30acc4bc0f827dee4b644339
Frankb-eng <55456165+Frankb-eng@users.noreply.github.com>
2019-10-14 08:33:10 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,52 +2,88 @@
 #include <stdio.h>
 #include <string.h>
 
-#define TEXTO_MAX 50
-#define LARGO_MAX 5
-#define LARGONOMBRE 19
+#define TEXTO_MAX 50    // Cantidad maxima de caracteres por linea
+#define LARGO_MAX 5     // Cantidad maxima de lineas por archivo
+#define LARGONOMBRE 19  // Texto mas extension
 
 using namespace std;
-
 #include "Definiciones.h"
 #include "Funciones.cpp"
 
-/*
-* TODO 2 getline dependiendo de nuestra entrada;
-* ver funciones que a cada uno nos funcione diferente/mal;
-* definir todos los tipos de variables y nombres de variables para cada estructura
+/** TODO CAT, UNDELETE, BC
 */
-int main(){
-
-    string comando;
-    Directorio d=NULL;
+int main()
+{
+    string comando,tipo,texto,subComando,nombre_archivo;
+    int espacio = 0,pos = 0;
+    Directorio d = NULL;
     d = new _directorio;
-    //d->nombreDirectorio='/';
     d->dirsig = NULL;
     d->contenido = NULL;
-    char nombre_archivo[LARGONOMBRE];
     bool flag = true;
-    while(flag == true){
-
+    while(flag == true)
+    {
         getline(cin,comando,'\n');
         cin.clear();
 
-        if(comando.compare("FIN") == 0){
-            flag = false;
-        }else{
-            getline(cin,comando,' ');
-            if(comando.compare("CREATE")==0){
-                cin.getline(nombre_archivo,19,'\n');
-                MuestroRetorno(CREATE(d,nombre_archivo));
-            }
+        pos = comando.find(' ');
+        tipo = comando.substr(0, pos);
+        subComando = comando.substr(pos + 1);
 
-            if(comando.compare("IF") == 0)
-            if(comando.compare("TYPE") == 0)
-            if(comando.compare("DELETE") == 0)
-            if(comando.compare("BF") == 0)
-            if(comando.compare("CAT") == 0)
-            if(comando.compare("IC") == 0)
-            if(comando.compare("BC") == 0)
-            if(comando.compare("UNDELETE") == 0)
+        if(tipo.compare("CREATE") == 0)
+        {
+            MuestroRetorno(CREATE(d,subComando));
+        }
+
+        if(tipo.compare("DIR") == 0)
+            MuestroRetorno(DIR(d));
+
+        if(tipo.compare("IF") == 0)
+        {
+            espacio = subComando.find(' ');
+            texto = subComando.substr(espacio + 1);
+            subComando = subComando.substr(0,espacio);
+            MuestroRetorno(IF(d,subComando,texto));
+        }
+
+        if(tipo.compare("TYPE") == 0)
+        {
+            MuestroRetorno(TYPE(d,subComando));
+        }
+
+        if(tipo.compare("DELETE") == 0)
+        {
+            MuestroRetorno(DELETE(d,subComando));
+        }
+
+        if(tipo.compare("UNDELETE") == 0)
+        {
+            MuestroRetorno(UNDELETE());
+        }
+        if(tipo.compare("BF") == 0)
+        {
+            espacio = subComando.find(' ');
+            subComando = subComando.substr(0,espacio);
+            MuestroRetorno(BF(d,subComando,0));
+        }
+
+        if(tipo.compare("CAT") == 0){}
+            //MuestroRetorno(CAT());
+        if(tipo.compare("IC") == 0)
+        {
+            espacio = subComando.find(' ');
+            texto = subComando.substr(espacio + 1);
+            subComando = subComando.substr(0,espacio);
+            MuestroRetorno(IC(d,subComando,texto));
+        }
+
+        if(tipo.compare("BC") == 0)
+        {
+            MuestroRetorno(BC());
+        }
+        if(tipo.compare("FIN") == 0)
+        {
+            flag = false;
         }
     }
     return 0;
