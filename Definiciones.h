0c3547476e827873eb07ca3c593a087f2db5e8f4
eugenio-perdomo <scnhp.0894@gmail.com>
2019-10-29 08:41:44 -0300

    Update Definiciones.h

--- Definiciones.h
+++ Definiciones.h
@@ -5,22 +5,26 @@ typedef enum _retorno
 {
     OK,ERROR,NO_IMPLEMENTADO
 } TipoRet;
+
 typedef char *Cadena;
 
 typedef struct _archivo
 {
     std::string nombreArchivo;
     Cadena contenido[LARGO_MAX];
-    _archivo *ptrsig;
+    //_archivo *ptrsig;
     int lineas;
+    _archivo *archizq;
+    _archivo *archder;
 }*Archivo;
 
 typedef struct _directorio
 {
     std::string nom;
     Archivo contenido;
-    //_directorio *subdir;
-    _directorio *dirsig;
+    _directorio *subdir;
+    _directorio *dirizq;
+    _directorio *dirder;
 }*Directorio;
 
 //tipo1
@@ -36,7 +40,7 @@ TipoRet CAT();
 
 //opcionales
 TipoRet IC(Directorio d, std::string nombreArchivo, string texto);
-TipoRet BC(Directorio d,string nombreArchivo, int linea);
+TipoRet BC();
 TipoRet UNDELETE();
 
 //otras
@@ -46,6 +50,14 @@ bool hayComillas(std::string texto);
 Directorio CrearArchivo(Directorio d, std::string nombre_archivo);
 Directorio eliminarArchivo(Directorio d, std::string nombre);
 bool esVacio(Archivo a);
+bool esVacioDir(Directorio d);
+Archivo buscoArchivo(Archivo a, string nom);
+Directorio buscoDirectortio(Directorio d, string nom);
+bool hojaArch(Archivo a);
+bool hojaDir(Directorio d);
+void muestroArchivos(Archivo a);
+void muestroDirectorios(Directorio d);
+void muestroTodo(Directorio raiz);
 void cargarDatosDePrueba(Directorio d);
 
 #endif // DEFINICIONES_H_INCLUDED
