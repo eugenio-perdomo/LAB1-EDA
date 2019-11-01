a163cecf805910990bc9e75e7a049ee392b132a4
Frankb-eng <55456165+Frankb-eng@users.noreply.github.com>
2019-11-01 09:13:21 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,8 +19,10 @@ int main()
     int espacio = 0,pos = 0;
     Directorio d = NULL;
     d = new _directorio;
+    d->nom="Directorio1";
     d->hijo = NULL;
     d->contenido = NULL;
+    cargarDatosDePrueba(d);
     bool flag = true;
     while(flag == true)
     {
