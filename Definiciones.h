f7428cbc66eddf982b38b30355df9349f7b71f80
eugenio-perdomo <scnhp.0894@gmail.com>
2019-11-01 18:59:52 -0700

    Cambio a la estructura de Dir y nuevas funciones
    La estructura de directorios pasan a ser arboles n-arios

--- Definiciones.h
+++ Definiciones.h
@@ -22,51 +22,48 @@ typedef struct _directorio
 {
     std::string nom;
     Archivo contenido;
-    _directorio *subdir;
-    _directorio *dirizq;
-    _directorio *dirder;
+    _directorio *hijo;
+    _directorio *padre;
+    _directorio *hermano;
 }*Directorio;
 
-//tipo1
+/// tipo1
 TipoRet DIR(Directorio d);
-TipoRet CREATE(Directorio d, std::string nombre_archivo);
-TipoRet IF(Directorio d, std::string nombreArchivo, std::string texto);
+TipoRet CREATE(Directorio &d, std::string nombre_archivo);
+TipoRet IF(Directorio &d, std::string nombreArchivo, std::string texto);
 TipoRet TYPE(Directorio d, std::string nombreArchivo);
-
-TipoRet MKDIR();
+TipoRet MKDIR(Directorio &d, std::string nombre_archivo);
 TipoRet CD();
 
-//tipo2
-TipoRet DELETE(Directorio d, std::string palabra);
-TipoRet BF(Directorio d,string nombreArchivo, int linea);
+/// tipo2
+TipoRet DELETE(Directorio &d, std::string palabra);
+TipoRet BF(Directorio &d,string nombreArchivo, int linea);
 TipoRet CAT();
-
-TipoRet PWD();
+TipoRet PWD(Directorio d);
 TipoRet RMDIR();
 
-//opcionales
-TipoRet IC(Directorio d, std::string nombreArchivo, string texto);
+/// opcionales
+TipoRet IC(Directorio &d, std::string nombreArchivo, string texto);
 TipoRet BC();
 TipoRet UNDELETE();
-
 TipoRet DIR_S();
 TipoRet COPY();
 
-//otras
+/// otras
 void MuestroRetorno(TipoRet ret);
 int tamanio(Archivo a);
 bool hayComillas(std::string texto);
 Directorio CrearArchivo(Directorio d, std::string nombre_archivo);
+Directorio CrearDirectorio(Directorio d, string padre, string nombre);
 Directorio eliminarArchivo(Directorio d, std::string nombre);
 bool esVacio(Archivo a);
-bool esVacioDir(Directorio d);
+bool esVacio2(Directorio d);
 Archivo buscoArchivo(Archivo a, string nom);
-Directorio buscoDirectortio(Directorio d, string nom);
+Directorio buscoDirectorio(Directorio d, string nombre);
 bool hojaArch(Archivo a);
 bool hojaDir(Directorio d);
 void muestroArchivos(Archivo a);
 void muestroDirectorios(Directorio d);
 void muestroTodo(Directorio raiz);
-void cargarDatosDePrueba(Directorio d);
-
+void cargarDatosDePrueba(Directorio &d);
 #endif // DEFINICIONES_H_INCLUDED
