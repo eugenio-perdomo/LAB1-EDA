18b3794fe4512a17d3b8666c311076d385c58bb0
Frankb-eng <55456165+Frankb-eng@users.noreply.github.com>
2019-11-04 09:36:37 -0300

    Update Definiciones.h

--- Definiciones.h
+++ Definiciones.h
@@ -23,54 +23,48 @@ typedef struct _directorio
     std::string nom;
     Archivo contenido;
     _directorio *hijo;
-    _directorio *hermanoizq;
-    _directorio *hermanoder;
+    _directorio *padre;
+    _directorio *hermano;
 }*Directorio;
 
-//tipo1
+/// tipo1
 TipoRet DIR(Directorio d);
 TipoRet CREATE(Directorio &d, std::string nombre_archivo);
 TipoRet IF(Directorio &d, std::string nombreArchivo, std::string texto);
 TipoRet TYPE(Directorio d, std::string nombreArchivo);
-
-TipoRet MKDIR();
+TipoRet MKDIR(Directorio &d, std::string nombre_archivo);
 TipoRet CD();
 
-
-//tipo2
+/// tipo2
 TipoRet DELETE(Directorio &d, std::string palabra);
 TipoRet BF(Directorio &d,string nombreArchivo, int linea);
 TipoRet CAT();
 TipoRet PWD(Directorio d, string nombreDirectorio);
-
 TipoRet RMDIR();
 
-
-
-//opcionales
+/// opcionales
 TipoRet IC(Directorio &d, std::string nombreArchivo, string texto);
 TipoRet BC();
 TipoRet UNDELETE();
-
 TipoRet DIR_S();
 TipoRet COPY();
 
-
-
-//otras
+/// otras
 void MuestroRetorno(TipoRet ret);
 int tamanio(Archivo a);
 bool hayComillas(std::string texto);
 Directorio CrearArchivo(Directorio d, std::string nombre_archivo);
+Directorio CrearDirectorio(Directorio d, string padre, string nombre);
 Directorio eliminarArchivo(Directorio d, std::string nombre);
 bool esVacio(Archivo a);
 bool esVacio2(Directorio d);
 Archivo buscoArchivo(Archivo a, string nom);
-Directorio buscoDirectortio(Directorio d, string nom);
+Directorio buscoDirectorio(Directorio d, string nombre);
 bool hojaArch(Archivo a);
 bool hojaDir(Directorio d);
 void muestroArchivos(Archivo a);
 void muestroDirectorios(Directorio d);
 void muestroTodo(Directorio raiz);
+Directorio recorrida(Directorio d, string texto);
 void cargarDatosDePrueba(Directorio &d);
-Directorio recorrida(Directorio d, string texto)
+#endif // DEFINICIONES_H_INCLUDED
