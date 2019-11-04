5944ce68209ed40e8f4572f0011acdf104589967
Frankb-eng <55456165+Frankb-eng@users.noreply.github.com>
2019-11-04 09:37:12 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
+
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+#include <algorithm>
 
 #define TEXTO_MAX 50    // Cantidad maxima de caracteres por linea
 #define LARGO_MAX 5     // Cantidad maxima de lineas por archivo
@@ -10,8 +12,9 @@ using namespace std;
 #include "Definiciones.h"
 #include "Funciones.cpp"
 
-/** TODO 
-    CAT, UNDELETE, DELETE, MKDIR, CD, PWD, RMDIR, DIR_S, COPY
+/** TODO
+    CAT, UNDELETE, DELETE, CD, RMDIR, DIR_S, COPY
+    MKDIR y PWD en proceso.
 */
 int main()
 {
@@ -19,8 +22,9 @@ int main()
     int espacio = 0,pos = 0;
     Directorio d = NULL;
     d = new _directorio;
-    d->nom="Directorio1";
+    d->nom = "/";
     d->hijo = NULL;
+    d->padre=NULL;
     d->contenido = NULL;
     cargarDatosDePrueba(d);
     bool flag = true;
@@ -28,15 +32,12 @@ int main()
     {
         getline(cin,comando,'\n');
         cin.clear();
-
         pos = comando.find(' ');
         tipo = comando.substr(0, pos);
         subComando = comando.substr(pos + 1);
 
         if(tipo.compare("CREATE") == 0)
-        {
             MuestroRetorno(CREATE(d,subComando));
-        }
 
         if(tipo.compare("DIR") == 0)
             MuestroRetorno(DIR(d));
@@ -50,19 +51,14 @@ int main()
         }
 
         if(tipo.compare("TYPE") == 0)
-        {
             MuestroRetorno(TYPE(d,subComando));
-        }
 
         if(tipo.compare("DELETE") == 0)
-        {
             MuestroRetorno(DELETE(d,subComando));
-        }
 
         if(tipo.compare("UNDELETE") == 0)
-        {
             MuestroRetorno(UNDELETE());
-        }
+
         if(tipo.compare("BF") == 0)
         {
             espacio = subComando.find(' ');
@@ -70,8 +66,8 @@ int main()
             MuestroRetorno(BF(d,subComando,0));
         }
 
-        if(tipo.compare("CAT") == 0){}
-            //MuestroRetorno(CAT());
+        if(tipo.compare("CAT") == 0) {}
+        //MuestroRetorno(CAT());
         if(tipo.compare("IC") == 0)
         {
             espacio = subComando.find(' ');
@@ -79,15 +75,19 @@ int main()
             subComando = subComando.substr(0,espacio);
             MuestroRetorno(IC(d,subComando,texto));
         }
+        if(tipo.compare("MKDIR") == 0)
+            MuestroRetorno(MKDIR(d,subComando));
+
+        if(tipo.compare("PWD") == 0)
+            MuestroRetorno(PWD(d,d->nom));
 
         if(tipo.compare("BC") == 0)
-        {
-            cout<<"BC";
-        }
+            cout << "BC";
+
         if(tipo.compare("FIN") == 0)
-        {
             flag = false;
-        }
+
     }
     return 0;
 }
+
