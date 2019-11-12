b819633fd03424731ec5c596e9903568a88cc3e7
eugenio-perdomo <scnhp.0894@gmail.com>
2019-11-12 05:09:51 -0800

    Añadidas algunas funciones de prueba

--- Definiciones.h
+++ Definiciones.h
@@ -33,12 +33,12 @@ TipoRet CREATE(Directorio &d, std::string nombre_archivo);
 TipoRet IF(Directorio &d, std::string nombreArchivo, std::string texto);
 TipoRet TYPE(Directorio d, std::string nombreArchivo);
 TipoRet MKDIR(Directorio &d, std::string nombre_archivo);
-TipoRet CD(Directorio d, string ruta);
+TipoRet CD(Directorio &d, string ruta);
 
 /// tipo2
 TipoRet DELETE(Directorio &d, std::string palabra);
 TipoRet BF(Directorio &d,string nombreArchivo, int linea);
-TipoRet CAT();
+TipoRet CAT(/*Directorio &d, string nombreArchivo1, string nombreArchivo2*/);
 TipoRet PWD(Directorio d);
 TipoRet RMDIR();
 
@@ -46,15 +46,16 @@ TipoRet RMDIR();
 TipoRet IC(Directorio &d, std::string nombreArchivo, string texto);
 TipoRet BC();
 TipoRet UNDELETE();
-TipoRet DIR_S();
+TipoRet DIR_S(Directorio d);
 TipoRet COPY();
 
 /// otras
 void MuestroRetorno(TipoRet ret);
+Directorio CreoDirectorio(Directorio d);
 int tamanio(Archivo a);
 bool hayComillas(std::string texto);
 Directorio CrearArchivo(Directorio d, std::string nombre_archivo);
-Directorio CrearDirectorio(Directorio d, string padre, string nombre);
+Directorio CrearDirectorio(Directorio &d, string padre, string nombre);
 Directorio eliminarArchivo(Directorio d, std::string nombre);
 bool esVacio(Archivo a);
 bool esVacio2(Directorio d);
@@ -64,11 +65,13 @@ Directorio buscoDirectorioHijo(Directorio d, string nombre);
 bool hojaArch(Archivo a);
 bool hojaDir(Directorio d);
 void muestroArchivos(Archivo a);
+void muestroArchivosDIRS(Archivo a);
 void muestroDirectorios(Directorio d);
 void muestroTodo(Directorio raiz);
-void cargarDatosDePrueba(Directorio d);
+void cargarDatosDePrueba(Directorio &d);
 Directorio recorrida(Directorio d, string texto);
-
 Directorio cargarDirectoriosDePrueba(Directorio d);
 Directorio buscoDirectorio(Directorio d, string nombre);
+Directorio irAraiz(Directorio d);
+void colorAlTexto();
 #endif // DEFINICIONES_H_INCLUDED
