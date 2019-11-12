75c7d126f9cbd8b70c8d6e7697aec4f0c7c2a0e1
eugenio-perdomo <scnhp.0894@gmail.com>
2019-11-12 11:31:21 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+#include <algorithm>
 
 #define TEXTO_MAX 50    // Cantidad maxima de caracteres por linea
 #define LARGO_MAX 5     // Cantidad maxima de lineas por archivo
@@ -10,33 +11,45 @@ using namespace std;
 #include "Definiciones.h"
 #include "Funciones.cpp"
 
-/** TODO CAT, UNDELETE
+/** TODO
+    CAT, UNDELETE, DELETE, RMDIR, COPY
+    MKDIR en proceso.
 */
 int main()
 {
     string comando,tipo,texto,subComando,nombre_archivo;
     int espacio = 0,pos = 0;
-    Directorio d = NULL;
-    d = new _directorio;
-    d->subdir = NULL;
-    d->contenido = NULL;
+    Directorio d = CreoDirectorio(d);
+    d = cargarDirectoriosDePrueba(d);
+    cargarDatosDePrueba(d);
     bool flag = true;
+
     while(flag == true)
     {
+        colorAlTexto();
         getline(cin,comando,'\n');
         cin.clear();
-
         pos = comando.find(' ');
         tipo = comando.substr(0, pos);
         subComando = comando.substr(pos + 1);
 
-        if(tipo.compare("CREATE") == 0)
+        if(tipo.compare("DIR") == 0)
         {
-            MuestroRetorno(CREATE(d,subComando));
+            if(subComando.compare("S") == 0)
+            {
+                MuestroRetorno(DIR_S(d));
+            }
+            else
+            {
+                MuestroRetorno(DIR(d));
+            }
         }
 
-        if(tipo.compare("DIR") == 0)
-            MuestroRetorno(DIR(d));
+
+        if(tipo.compare("CREATE") == 0)
+            MuestroRetorno(CREATE(d,subComando));
+
+
 
         if(tipo.compare("IF") == 0)
         {
@@ -47,19 +60,14 @@ int main()
         }
 
         if(tipo.compare("TYPE") == 0)
-        {
             MuestroRetorno(TYPE(d,subComando));
-        }
 
         if(tipo.compare("DELETE") == 0)
-        {
             MuestroRetorno(DELETE(d,subComando));
-        }
 
         if(tipo.compare("UNDELETE") == 0)
-        {
             MuestroRetorno(UNDELETE());
-        }
+
         if(tipo.compare("BF") == 0)
         {
             espacio = subComando.find(' ');
@@ -67,8 +75,9 @@ int main()
             MuestroRetorno(BF(d,subComando,0));
         }
 
-        if(tipo.compare("CAT") == 0){}
-            //MuestroRetorno(CAT());
+        if(tipo.compare("CAT") == 0)
+            MuestroRetorno(CAT());
+
         if(tipo.compare("IC") == 0)
         {
             espacio = subComando.find(' ');
@@ -76,15 +85,34 @@ int main()
             subComando = subComando.substr(0,espacio);
             MuestroRetorno(IC(d,subComando,texto));
         }
+        if(tipo.compare("MKDIR") == 0)
+        {
+            espacio = subComando.find(' ');
+            texto = subComando.substr(espacio + 1);
+            subComando = subComando.substr(0,espacio);
+            MuestroRetorno(MKDIR(d,subComando));
+        }
+
+
+        if(tipo.compare("PWD") == 0)
+            MuestroRetorno(PWD(d));
+
 
         if(tipo.compare("BC") == 0)
-        {
-            cout<<"BC";
-        }
+            cout << "BC";
+        //MuestroRetorno(BC());
+
         if(tipo.compare("FIN") == 0)
-        {
             flag = false;
+
+        if(tipo.compare("CD") == 0)
+        {
+            espacio = subComando.find(' ');
+            texto = subComando.substr(espacio + 1);
+            subComando = subComando.substr(0,espacio);
+            MuestroRetorno(CD(d,subComando));
         }
+
     }
     return 0;
 }
