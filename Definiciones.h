12e24c346dfb3f314311214abe9519d11bcd6c10
eugenio-perdomo <scnhp.0894@gmail.com>
2019-11-12 11:30:08 -0300

    Update Definiciones.h

--- Definiciones.h
+++ Definiciones.h
@@ -18,61 +18,61 @@ typedef struct _archivo
     _archivo *archder;
 }*Archivo;
 
-typedef struct _directorio
+typedef struct _directorio      //  /   Dir1  -> Dir11  -> Dir12 ---  Dir2 -> Dir21  -> Dir22
 {
     std::string nom;
     Archivo contenido;
-    _directorio *subdir;
-    _directorio *dirizq;
-    _directorio *dirder;
+    _directorio *hijo;
+    _directorio *padre;
+    _directorio *hermano;
 }*Directorio;
 
-//tipo1
+/// tipo1
 TipoRet DIR(Directorio d);
-TipoRet CREATE(Directorio d, std::string nombre_archivo);
-TipoRet IF(Directorio d, std::string nombreArchivo, std::string texto);
+TipoRet CREATE(Directorio &d, std::string nombre_archivo);
+TipoRet IF(Directorio &d, std::string nombreArchivo, std::string texto);
 TipoRet TYPE(Directorio d, std::string nombreArchivo);
+TipoRet MKDIR(Directorio &d, std::string nombre_archivo);
+TipoRet CD(Directorio &d, string ruta);
 
-TipoRet MKDIR();
-TipoRet CD();
-
-
-//tipo2
-TipoRet DELETE(Directorio d, std::string palabra);
-TipoRet BF(Directorio d,string nombreArchivo, int linea);
-TipoRet CAT();
-
-TipoRet PWD();
+/// tipo2
+TipoRet DELETE(Directorio &d, std::string palabra);
+TipoRet BF(Directorio &d,string nombreArchivo, int linea);
+TipoRet CAT(/*Directorio &d, string nombreArchivo1, string nombreArchivo2*/);
+TipoRet PWD(Directorio d);
 TipoRet RMDIR();
 
-
-
-//opcionales
-TipoRet IC(Directorio d, std::string nombreArchivo, string texto);
+/// opcionales
+TipoRet IC(Directorio &d, std::string nombreArchivo, string texto);
 TipoRet BC();
 TipoRet UNDELETE();
-
-TipoRet DIR_S();
+TipoRet DIR_S(Directorio d);
 TipoRet COPY();
 
-
-
-//otras
+/// otras
 void MuestroRetorno(TipoRet ret);
+Directorio CreoDirectorio(Directorio d);
 int tamanio(Archivo a);
 bool hayComillas(std::string texto);
 Directorio CrearArchivo(Directorio d, std::string nombre_archivo);
+Directorio CrearDirectorio(Directorio &d, string padre, string nombre);
 Directorio eliminarArchivo(Directorio d, std::string nombre);
 bool esVacio(Archivo a);
 bool esVacio2(Directorio d);
 Archivo buscoArchivo(Archivo a, string nom);
-Directorio buscoDirectortio(Directorio d, string nom);
+Directorio buscoDirectorioHermano(Directorio d, string nombre);
+Directorio buscoDirectorioHijo(Directorio d, string nombre);
 bool hojaArch(Archivo a);
 bool hojaDir(Directorio d);
 void muestroArchivos(Archivo a);
+void muestroArchivosDIRS(Archivo a);
 void muestroDirectorios(Directorio d);
 void muestroTodo(Directorio raiz);
-
-void cargarDatosDePrueba(Directorio d);
-
+void cargarDatosDePrueba(Directorio &d);
+Directorio recorrida(Directorio d, string texto);
+Directorio cargarDirectoriosDePrueba(Directorio d);
+Directorio buscoDirectorio(Directorio d, string nombre);
+Directorio irAraiz(Directorio d);
+void moverseATodos(Directorio d);
+void colorAlTexto();
 #endif // DEFINICIONES_H_INCLUDED
