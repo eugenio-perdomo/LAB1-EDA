b1c1566a4b50d1a4c1d6da63046b63b9dd82b31d
eugenio-perdomo <scnhp.0894@gmail.com>
2019-11-18 10:23:43 -0300

    Limpieza y agragado de funciones

--- Definiciones.h
+++ Definiciones.h
@@ -6,13 +6,10 @@ typedef enum _retorno
     OK,ERROR,NO_IMPLEMENTADO
 } TipoRet;
 
-typedef char *Cadena;
-
 typedef struct _archivo
 {
-    std::string nombreArchivo;
-    Cadena contenido[LARGO_MAX];
-    _archivo *ptrsig;
+    string nombreArchivo;
+    string contenido[LARGO_MAX];
     int lineas;
     _archivo *archizq;
     _archivo *archder;
@@ -20,7 +17,7 @@ typedef struct _archivo
 
 typedef struct _directorio      //  /   Dir1  -> Dir11  -> Dir12 ---  Dir2 -> Dir21  -> Dir22
 {
-    std::string nom;
+    string nom;
     Archivo contenido;
     _directorio *hijo;
     _directorio *padre;
@@ -29,34 +26,33 @@ typedef struct _directorio      //  /   Dir1  -> Dir11  -> Dir12 ---  Dir2 -> Di
 
 /// tipo1
 TipoRet DIR(Directorio d);
-TipoRet CREATE(Directorio &d, std::string nombre_archivo);
-TipoRet IF(Directorio &d, std::string nombreArchivo, std::string texto);
-TipoRet TYPE(Directorio d, std::string nombreArchivo);
-TipoRet MKDIR(Directorio &d, std::string nombre_archivo);
+TipoRet CREATE(Directorio &d, string nombre_archivo);
+TipoRet IF(Directorio &d, string nombreArchivo, string texto);
+TipoRet TYPE(Directorio d, string nombreArchivo);
+TipoRet MKDIR(Directorio &d, string nombre_archivo);
 TipoRet CD(Directorio &d, string ruta);
 
 /// tipo2
-TipoRet DELETE(Directorio &d, std::string palabra);
+TipoRet DELETE(Directorio &d, string palabra);
 TipoRet BF(Directorio &d,string nombreArchivo, int linea);
-TipoRet CAT(/*Directorio &d, string nombreArchivo1, string nombreArchivo2*/);
+TipoRet CAT(Directorio &d, string nombreArchivo1, string nombreArchivo2);
 TipoRet PWD(Directorio d);
 TipoRet RMDIR();
 
 /// opcionales
-TipoRet IC(Directorio &d, std::string nombreArchivo, string texto);
-TipoRet BC();
+TipoRet IC(Directorio &d, string nombreArchivo, string texto);
+TipoRet BC(Directorio &d,string nombreArchivo, int linea);
 TipoRet UNDELETE();
 TipoRet DIR_S(Directorio d);
 TipoRet COPY();
 
 /// otras
 void MuestroRetorno(TipoRet ret);
-Directorio CreoDirectorio(Directorio d);
+Directorio CreoDirectorio();
 int tamanio(Archivo a);
-bool hayComillas(std::string texto);
-Directorio CrearArchivo(Directorio d, std::string nombre_archivo);
+bool hayComillas(string texto);
+Directorio CrearArchivo(Directorio d, string nombre_archivo);
 Directorio CrearDirectorio(Directorio &d, string padre, string nombre);
-Directorio eliminarArchivo(Directorio d, std::string nombre);
 bool esVacio(Archivo a);
 bool esVacio2(Directorio d);
 Archivo buscoArchivo(Archivo a, string nom);
@@ -72,7 +68,9 @@ void cargarDatosDePrueba(Directorio &d);
 Directorio recorrida(Directorio d, string texto);
 Directorio cargarDirectoriosDePrueba(Directorio d);
 Directorio buscoDirectorio(Directorio d, string nombre);
+Directorio recorrerDirectorioHermano(Directorio d);
 Directorio irAraiz(Directorio d);
 void moverseATodos(Directorio d);
 void colorAlTexto();
+Archivo buscoMenor(Archivo a);
 #endif // DEFINICIONES_H_INCLUDED
