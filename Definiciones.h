64a7a42c6ee1dce9e23abfd240d393e5b9a1e929
Frankb-eng <55456165+Frankb-eng@users.noreply.github.com>
2019-11-22 08:23:55 -0300

    Update Definiciones.h

--- Definiciones.h
+++ Definiciones.h
@@ -8,7 +8,7 @@ typedef enum _retorno
 
 typedef struct _archivo
 {
-    std::string nombreArchivo;
+    string nombreArchivo;
     string contenido[LARGO_MAX];
     int lineas;
     _archivo *archizq;
@@ -17,7 +17,7 @@ typedef struct _archivo
 
 typedef struct _directorio      //  /   Dir1  -> Dir11  -> Dir12 ---  Dir2 -> Dir21  -> Dir22
 {
-    std::string nom;
+    string nom;
     Archivo contenido;
     _directorio *hijo;
     _directorio *padre;
@@ -26,21 +26,21 @@ typedef struct _directorio      //  /   Dir1  -> Dir11  -> Dir12 ---  Dir2 -> Di
 
 /// tipo1
 TipoRet DIR(Directorio d);
-TipoRet CREATE(Directorio &d, std::string nombre_archivo);
-TipoRet IF(Directorio &d, std::string nombreArchivo, std::string texto);
-TipoRet TYPE(Directorio d, std::string nombreArchivo);
-TipoRet MKDIR(Directorio &d, std::string nombre_archivo);
-TipoRet CD(Directorio &d, string ruta);
+TipoRet CREATE(Directorio &d, string nombre_archivo);
+TipoRet IF(Directorio &d, string nombreArchivo, string texto);
+TipoRet TYPE(Directorio d, string nombreArchivo);
+TipoRet MKDIR(Directorio &d, string nombre_archivo);
+TipoRet CD(Directorio &d, string camino);
 
 /// tipo2
 TipoRet DELETE(Directorio &d, std::string palabra, Archivo &repuesto, string &camino);
 TipoRet BF(Directorio &d,string nombreArchivo, int linea);
-TipoRet CAT(/*Directorio &d, string nombreArchivo1, string nombreArchivo2*/);
+TipoRet CAT(Directorio &d, string nombreArchivo1, string nombreArchivo2);
 TipoRet PWD(Directorio d);
 TipoRet RMDIR();
 
 /// opcionales
-TipoRet IC(Directorio &d, std::string nombreArchivo, string texto);
+TipoRet IC(Directorio &d, string nombreArchivo, string texto);
 TipoRet BC(Directorio &d,string nombreArchivo, int linea);
 TipoRet UNDELETE(Directorio &d, Archivo repuesto, string camino);
 TipoRet DIR_S(Directorio d);
@@ -48,12 +48,11 @@ TipoRet COPY();
 
 /// otras
 void MuestroRetorno(TipoRet ret);
-Directorio CreoDirectorio(Directorio d);
+Directorio CreoDirectorio();
 int tamanio(Archivo a);
-bool hayComillas(std::string texto);
-Directorio CrearArchivo(Directorio d, std::string nombre_archivo);
+bool hayComillas(string texto);
+Directorio CrearArchivo(Directorio d, string nombre_archivo);
 Directorio CrearDirectorio(Directorio &d, string padre, string nombre);
-Directorio eliminarArchivo(Directorio d, std::string nombre);
 bool esVacio(Archivo a);
 bool esVacio2(Directorio d);
 Archivo buscoArchivo(Archivo a, string nom);
@@ -69,9 +68,9 @@ void cargarDatosDePrueba(Directorio &d);
 Directorio recorrida(Directorio d, string texto);
 Directorio cargarDirectoriosDePrueba(Directorio d);
 Directorio buscoDirectorio(Directorio d, string nombre);
+Directorio recorrerDirectorioHermano(Directorio d);
 Directorio irAraiz(Directorio d);
 void moverseATodos(Directorio d);
 void colorAlTexto();
 Archivo buscoMenor(Archivo a);
 #endif // DEFINICIONES_H_INCLUDED
-
